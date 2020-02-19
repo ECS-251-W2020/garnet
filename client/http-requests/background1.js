bf2828adaf10d0cc18b5247d3b867dc05bca1dd6
YUNQIUGUO <35738743+YUNQIUGUO@users.noreply.github.com>
2020-02-18 19:30:34 -0800

    intercept_http_requests

---
+++ client/http-requests/background1.js
@@ -0,0 +1,10 @@
+function logURL(requestDetails) {
+    console.log("Loading: " + requestDetails.url);
+  }
+  
+  browser.webRequest.onBeforeRequest.addListener(
+    logURL,
+    {urls: ["<all_urls>"]}
+  );
+  // intercept all URL requests
+  
