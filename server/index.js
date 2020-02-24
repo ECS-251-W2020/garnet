cd18ba1ab1a35524a839eed1a46d1bda5f3ae1d7
Pengcheng Ding <info@desmonding.me>
2020-02-23 23:33:36 -0800

    Setup client-side socket.io demo and get draw commands from server

--- server/index.js
+++ server/index.js
@@ -1,8 +1,12 @@
 const Koa = require('koa')
 const SocketIO = require('koa-socket-2')
+const serve = require('koa-static')
+
 const app = new Koa()
 const io = new SocketIO()
 const PORT = 3000
+
+app.use(serve('client'))
 
 app.use(ctx => {
   ctx.body = 'Hello World!'
@@ -10,12 +14,20 @@ app.use(ctx => {
 
 io.attach(app)
 
+io.on('connection', () => {
+  console.log('a client connected')
+})
+
+io.on('disconnect', () => {
+  console.log('a client disconnected')
+})
+
 io.on('message', (ctx, data) => {
-  console.log('client sent data to message endpoint', data)
+  console.log('client sent data to message endpoint: ', data)
 
   // Deliver mock draw commands to client for testing
-  const commands = require('./mocks/drawTextCommands')
-  ctx.socket.broadcast.emit('message', commands)
+  const commands = require('./mocks/drawTextCommands')(data)
+  io.broadcast('message', commands)
 })
 
 console.log(`Listening on port: ${PORT}`)
