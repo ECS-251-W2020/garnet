080e7e94aa820edb8c518f8ffce44d3512a102ce
Pengcheng Ding <info@desmonding.me>
2020-02-25 11:15:29 -0800

    Replay dynamic draw commands

--- client/demo/draw-text/index.js
+++ client/demo/draw-text/index.js
@@ -1,22 +1,14 @@
 const socket = io()
 const form = document.querySelector('form')
 const input = document.querySelector('input')
+const canvas = document.getElementById('main')
 
 form.addEventListener('submit', onSubmit)
+window.addEventListener('load', resizeCanvas)
+window.addEventListener('resize', resizeCanvas)
 
-socket.on('message', drawText)
-
-function onSubmit (event) {
-  event.preventDefault()
-  socket.emit('message', input.value)
-  input.value = ''
-}
-
-function drawText (data) {
-  console.log(data)
-
-  // TODO(pengcheng): replay drawText commands
-}
+let x = 100
+let y = 100
 
 // draw simple text using CanvasKit (skia + webAssembly)
 CanvasKitInit({
@@ -24,70 +16,33 @@ CanvasKitInit({
 })
   .ready()
   .then(CanvasKit => {
-    let robotoData = null
-    fetch(
-      'https://storage.googleapis.com/skia-cdn/google-web-fonts/Roboto-Regular.ttf'
-    ).then(resp => {
-      resp.arrayBuffer().then(buffer => {
-        robotoData = buffer
-        requestAnimationFrame(drawFrame)
-      })
-    }) // fetch font
+    socket.on('message', drawText)
 
     const surface = CanvasKit.MakeCanvasSurface('main')
+
     if (!surface) {
       throw new Error('Could not make surface')
     }
 
-    const skcanvas = surface.getCanvas()
+    const skCanvas = surface.getCanvas()
+    const skFont = new CanvasKit.SkFont(null, 18)
+    const skPaint = new CanvasKit.SkPaint()
+    let drawCommands = ''
 
-    const font = new CanvasKit.SkFont(null, 18)
-    const fontPaint = new CanvasKit.SkPaint()
-    fontPaint.setStyle(CanvasKit.PaintStyle.Fill)
-    fontPaint.setAntiAlias(true)
-
-    skcanvas.drawText(`Fetching Font data...`, 5, 450, fontPaint, font)
-    surface.flush()
-
-    const context = CanvasKit.currentContext()
-
-    let paragraph = null
-    const str =
-      'Our group project is to accomplish a new remote browser isolation system with the help of chromium engine.'
-
-    function drawFrame () {
-      if (robotoData && !paragraph) {
-        const fontMgr = CanvasKit.SkFontMgr.FromData([robotoData])
-
-        const paraStyle = new CanvasKit.ParagraphStyle({
-          textStyle: {
-            color: CanvasKit.BLACK,
-            fontFamilies: ['Roboto'],
-            fontSize: 50
-          },
-          textAlign: CanvasKit.TextAlign.Left,
-          maxLines: 7,
-          ellipsis: '...'
-        })
-
-        const builder = CanvasKit.ParagraphBuilder.Make(paraStyle, fontMgr)
-        builder.addText(str)
-        paragraph = builder.build()
-      }
-
-      if (!paragraph) {
-        requestAnimationFrame(drawFrame)
-        return
-      }
-
-      CanvasKit.setCurrentContext(context)
-      skcanvas.clear(CanvasKit.WHITE)
-
-      const wrapTo = 350 + 150 * Math.sin(Date.now() / 1000)
-      paragraph.layout(wrapTo)
-      skcanvas.drawParagraph(paragraph, 0, 0)
-
-      surface.flush()
-      requestAnimationFrame(drawFrame)
+    function drawText(commands) {
+      drawCommands += commands
+      eval(drawCommands)
     }
   })
+
+function onSubmit(event) {
+  event.preventDefault()
+  y += 50
+  socket.emit('message', { text: input.value, x, y })
+  input.value = ''
+}
+
+function resizeCanvas() {
+  canvas.width = window.innerWidth
+  canvas.height = window.innerHeight
+}
