a3b5cdf11e3ba4fa22bc24a838d1db7f2e1f59b9
Pengcheng Ding <pcding@ucdavis.edu>
2020-03-07 14:45:41 -0800

    Use puppeteer to start chromium instance

--- server/index.js
+++ server/index.js
@@ -1,16 +1,31 @@
 const Koa = require('koa')
 const SocketIO = require('koa-socket-2')
 const serve = require('koa-static')
+const puppeteer = require('puppeteer-core')
 
 const app = new Koa()
 const io = new SocketIO()
 const PORT = 3000
+const CHROMIUM_PATH =
+  '/Users/Desmond/repo/chromium/out/Default/Chromium.app/Contents/MacOS/Chromium'
 
 app.use(serve('.'))
 
 app.use(ctx => {
   ctx.body = 'Hello World!'
 })
+
+const startBrowser = async () => {
+  const browser = await puppeteer.launch({
+    executablePath: CHROMIUM_PATH
+  })
+  const page = await browser.newPage()
+  await page.goto('https://duckduckgo.com')
+  await page.screenshot({ path: 'example.png' })
+  await browser.close()
+}
+
+startBrowser()
 
 io.attach(app)
 
